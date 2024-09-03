d031a9f9787c6a4b34212f0c33691c12560c217d
martinbetancur <martinjavieli@gmail.com>
2024-09-02 22:00:21 -0500

    Tutorial 3

--- pages/apps.py
+++ pages/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from django.utils.module_loading import import_string
-from django.conf import settings
-
+from django.utils.module_loading import import_string 
+from django.conf import settings 
 
 class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
